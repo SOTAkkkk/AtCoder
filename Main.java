148558f009d0cd7a6e31797e07b16ba066458bca
SOTAkkkk <snssota@gmail.com>
2022-05-14 15:35:23 +0900

    ABC228-A

--- Main.java
+++ Main.java
@@ -3,19 +3,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String S1 = scanner.nextLine();
-        String S2 = scanner.nextLine();
-
-        char A1 = S1.charAt(0);
-        char A2 = S1.charAt(1);
-        char A3 = S2.charAt(0);
-        char A4 = S2.charAt(1);
-
-        if ((A1 == '.' && A4 == '.') || (A2 == '.' && A3 == '.')) {
+        int S = scanner.nextInt();
+        int T = scanner.nextInt();
+        int X = scanner.nextInt();
+        if (S > T) {
+            if ((S <= X && X <= 23) || (0 <= X && X < T)) {
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
+        } else if (S <= X && X < T) {
+            System.out.println("Yes");
+        } else {
             System.out.println("No");
         }
-        else{
-            System.out.println("Yes");
-        }
+
     }
 }

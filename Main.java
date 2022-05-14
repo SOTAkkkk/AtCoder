a6a6616dec40c9367701a07ecf0aca6fa91cc3af
SOTAkkkk <snssota@gmail.com>
2022-05-14 16:18:27 +0900

    ABC235-A

--- Main.java
+++ Main.java
@@ -3,21 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int S = scanner.nextInt();
-        int T = scanner.nextInt();
-        int X = scanner.nextInt();
-        if (S > T) {
-            if ((S <= X && X <= 23) || (0 <= X && X < T)) {
-                System.out.println("Yes");
-            }
-            else{
-                System.out.println("No");
-            }
-        } else if (S <= X && X < T) {
-            System.out.println("Yes");
-        } else {
-            System.out.println("No");
-        }
+        String S = scanner.nextLine();
+        int a = Character.getNumericValue(S.charAt(0));
+        int b = Character.getNumericValue(S.charAt(1));
+        int c = Character.getNumericValue(S.charAt(2));
 
+        int inputNumber = Integer.parseInt(S);
+        int firstPlace=inputNumber;
+        int ans = 0;
+        while (firstPlace != 0) {
+            firstPlace = inputNumber % 10;
+            ans += firstPlace * 100 + firstPlace * 10 + firstPlace;
+            inputNumber /= 10;
+        }
+        System.out.println(ans);
     }
 }

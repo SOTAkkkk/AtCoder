6fc4370f4691fea3dfa3169336e5da190b67f49b
SOTAkkkk <snssota@gmail.com>
2022-05-15 21:11:54 +0900

    ABC219-A

--- Main.java
+++ Main.java
@@ -3,19 +3,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
-        int C = scanner.nextInt();
+        int X = scanner.nextInt();
 
-        int i = 1;
-        while (C <= B) {
-            C *= i;
-            if (A <= C && C <= B) {
-                System.out.println(C);
-                return;
-            }
-            i++;
+        if (90 <= X) {
+            System.out.println("expert");
+        } else if (70 <= X) {
+            System.out.println(90 - X);
+        } else if (40 <= X) {
+            System.out.println(70 - X);
+        } else {
+            System.out.println(40 - X);
         }
-        System.out.println(-1);
     }
 }

a1b202a9a0340bad9c9f1343f2eff214f848b045
SOTAkkkk <snssota@gmail.com>
2022-05-15 22:08:27 +0900

    ABC215-A

--- Main.java
+++ Main.java
@@ -3,15 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double input = scanner.nextDouble();
-        int X = (int) Math.floor(input);
-        int Y = (int) ((input - (double) X)*10);
-        if (0 <= Y && Y <= 2) {
-            System.out.println(X + "-");
-        } else if (3 <= Y && Y <= 6) {
-            System.out.println(X);
-        } else {
-            System.out.println(X + "+");
+        String S = scanner.nextLine();
+
+        if(S.equals("Hello,World!")){
+            System.out.println("AC");
+        }
+        else{
+            System.out.println("WA");
         }
     }
 }

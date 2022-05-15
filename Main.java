a07c7bb252615413e6944730615999ec1e8ff8ba
SOTAkkkk <snssota@gmail.com>
2022-05-15 21:29:28 +0900

    ABC218-A

--- Main.java
+++ Main.java
@@ -3,16 +3,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int X = scanner.nextInt();
-
-        if (90 <= X) {
-            System.out.println("expert");
-        } else if (70 <= X) {
-            System.out.println(90 - X);
-        } else if (40 <= X) {
-            System.out.println(70 - X);
+        int N = scanner.nextInt();
+        scanner.nextLine();
+        String S = scanner.nextLine();
+        if (S.charAt(N - 1) == 'o') {
+            System.out.println("Yes");
         } else {
-            System.out.println(40 - X);
+            System.out.println("No");
         }
+
     }
 }

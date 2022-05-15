09c7fd72b4f0012bbca2aca82e94f2fec2546669
SOTAkkkk <snssota@gmail.com>
2022-05-15 18:06:00 +0900

    ABC224-A

--- Main.java
+++ Main.java
@@ -1,26 +1,15 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String S = scanner.nextLine();
 
-        HashSet hashSet = new HashSet();
-        for(int i = 0; i<3;i++){
-            hashSet.add(S.charAt(i));
+        if(S.endsWith("er")){
+            System.out.println("er");
         }
-        if(hashSet.size()==1){
-            System.out.println(1);
-        }
-        else if (hashSet.size() == 2){
-            System.out.println(3);
-        }
-        else {
-            System.out.println(6);
+        else{
+            System.out.println("ist");
         }
     }
 }

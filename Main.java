30c714bb045a238d4ffc3d0647c48129447ed20b
SOTAkkkk <snssota@gmail.com>
2022-05-15 21:03:00 +0900

    ABC219-A

--- Main.java
+++ Main.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String S = scanner.nextLine();
-        if(S.length() == 4){
-            System.out.println(S);
-        }
-        else if (S.length() == 3){
-            System.out.println("0"+S);
-        }
-        else if (S.length() == 2 ){
-            System.out.println("0"+"0"+S);
-        }
-        else {
-            System.out.println("0"+"0"+"0"+S);
-        }
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int C = scanner.nextInt();
 
-
+        int i = 1;
+        while (C <= B) {
+            C *= i;
+            if (A <= C && C <= B) {
+                System.out.println(C);
+                return;
+            }
+            i++;
+        }
+        System.out.println(-1);
     }
 }

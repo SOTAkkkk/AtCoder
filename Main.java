b213e002489e43b9b8b9390c8a384f05ff1ba5c0
SOTAkkkk <snssota@gmail.com>
2022-05-15 20:26:31 +0900

    ABC-222

--- Main.java
+++ Main.java
@@ -4,12 +4,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String S = scanner.nextLine();
+        if(S.length() == 4){
+            System.out.println(S);
+        }
+        else if (S.length() == 3){
+            System.out.println("0"+S);
+        }
+        else if (S.length() == 2 ){
+            System.out.println("0"+"0"+S);
+        }
+        else {
+            System.out.println("0"+"0"+"0"+S);
+        }
 
-        if(S.endsWith("er")){
-            System.out.println("er");
-        }
-        else{
-            System.out.println("ist");
-        }
+
     }
 }
